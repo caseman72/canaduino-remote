a6fa70942bac2d6874d2c99c083a4f655b38eecd
Casey Manion <casey@manion.com>
2026-05-07 00:23:40 -0700

    removing shop-controller files

--- shop-controller/main/ota.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/* Start HTTP OTA server on port 8080.
- * Upload firmware: curl -X POST --data-binary @firmware.bin http://<ip>:8080/upload
- * Device reboots automatically after successful upload. */
-void ota_init(void);
--- shop-controller/main/temp.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-/* Initialize DS18B20 1-Wire sensor and start periodic reads.
- * Publishes temperature to MQTT every 60 seconds. */
-void temp_init(void);
